d4d7aeb5ce94ab26f3c5ebf64bcb8da697af059a
Luciano Garriga <lucianomgarriga@gmail.com>
2022-09-30 15:28:13 -0300

    Initializing firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhKuziruyBUAAYGyMLUvSDZ8OJjadbUp0",
+    authDomain: "to-do-list-c459f.firebaseapp.com",
+    projectId: "to-do-list-c459f",
+    storageBucket: "to-do-list-c459f.appspot.com",
+    messagingSenderId: "554241689712",
+    appId: "1:554241689712:web:efc5388dc6c758e7a04cb5",
+    measurementId: "G-6YKREC4MLC"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.getAnalytics();
+  const db = getFirestore();
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function addItem(event){
     event.preventDefault();
     let text = document.getElementById('todo-input');
-    console.log(text.value);
+    
+    db.collection("todo-items").add({
+        text: text.value,
+        status: "active"
+    })
+    text.value = '';
 }
