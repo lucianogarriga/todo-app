5996a3663d5065b19e2b8076996ffd70174990bd
Luciano Garriga <lucianomgarriga@gmail.com>
2022-10-08 15:42:59 -0300

    Modifying some files to interact

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const theme = document.getElementById('theme');
+const newItemInput = document.getElementById('addItem');
+const todoList = document.querySelector('.content ul')
 
 theme.addEventListener('click', ()=>{
     document.querySelector('body').classList = [theme.checked ? 'theme-light' : 'theme-dark'];
 })
+
+newItemInput.addEventListener('keypress', (e) =>{
+    if (e.charCode === 13 && newItemInput.value.length > 0){
+        createNewTodoItem(newItemInput.value)
+    }
+})
+
+ function createNewTodoItem(text){
+    const element = document.createElement('li');
+
+    element.innerHTML = `
+    <label class="list-item">
+        <input type="checkbox" name="todoItem1">
+        <span class="checkmark"></span>
+        <span class="text">${text}</span>
+    </label> 
+    <span class="remove"></span>
+    `;
+
+    todoList.append(element); 
+}
